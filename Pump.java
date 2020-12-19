92e33db2f27402ae05c9c7eb7109f78d4b212cb1
Alex Kaprosy <afkaprosy@gmail.com>
2020-12-19 06:24:25 -0800

    Add files via upload

---
+++ Pump.java
@@ -0,0 +1,12 @@
+
+public class Pump {
+	double pressure;
+	double flow;
+	double caseDrain;
+	double displacement;
+	double speed;
+	
+	static void pressure() {
+		System.out.println("3000psi");
+}
+}
