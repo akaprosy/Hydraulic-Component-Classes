99ae608e994ddc193e5b888f8311d60ccdb2de7e
Alex Kaprosy <afkaprosy@gmail.com>
2020-12-19 06:35:41 -0800

    Add files via upload

--- Pump.java
+++ Pump.java
@@ -1,12 +1,32 @@
 
 public class Pump {
 	double pressure;
-	double flow;
 	double caseDrain;
 	double displacement;
-	double speed;
+	double rpm;
 	
-	static void pressure() {
-		System.out.println("3000psi");
+	public Pump(double pressure, double caseDrain, double displacement, double rpm) {
+		this.pressure = pressure;
+		this.caseDrain = caseDrain;
+		this.displacement = displacement;
+		this.rpm = rpm;
+	}
+	public void pressure() {
+		System.out.println("Pressure equals: " + pressure);
 }
+	public double flow() {
+		double flow = (displacement*rpm)/231;
+		return flow;
+	}
+	
+	public double horsepower() {
+		double flowHP = flow();
+		double horsePower = (pressure*flowHP)/1714;
+		return horsePower;
+	}
+	//variable output is the actual flow measured with a flow meter(outside the program)
+	public double pumpEfficiency(double output) {
+		double VolEfficiency = ((output*231)/(displacement*rpm))*100;
+		return VolEfficiency;
+	}
 }
